08269a6f2dbf4e123f35e8423effb8931c6d1f65
dorkwitafork <hawks965@gmail.com>
2026-09-07 19:51:08 -0700

    playing with some simple requests

--- pokeapp.py
+++ pokeapp.py
@@ -1,1 +1,11 @@
 import requests
+
+url = "https://pokeapi.co/api/v2"
+
+response = requests.get(url)
+
+print(f"Status code: {response.status_code}")
+print(f"Content Type: {response.headers.get("Content-Type")}")
+
+data = response.json()
+print(data)
