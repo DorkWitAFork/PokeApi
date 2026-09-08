c7c3119bf10971e0d8f8e595d1248896e9005a6b
dorkwitafork <hawks965@gmail.com>
2026-09-08 10:51:09 -0700

    WiP: team selection

--- pokeapp.py
+++ pokeapp.py
@@ -10,10 +10,22 @@ while True:
     if team_size >= 1 and team_size <= 6:
         break
     print("Error, you need to have at least one and no more than six!")
-for i in range(1, team_size+1):
-    print(i)
 
-pokemon = input("Enter a pokemon you'd like to see: ")
+# Build the team by inputting names of pokemon, which are then looked up.
+team = []
+print("~~~ CHOOSE YOUR POKEMON ~~~\n")
+for i in range(0, team_size):
+    pokemon = input(f"Enter your choice for Pokemon #{i+1}: ")
+    try:
+        response = requests.get(f"{url}/pokemon/{pokemon}", timeout = 10)
+        print("Final url: ", response.url)
+    except requests.exceptions.Timeout:
+        print("Request timed out")
+    except requests.exceptions.RequestException as e:
+        print("Requested failed: ", e)
+    team.append(pokemon)
+
+print(f"Here is your team: {team}")
 
 
 try:
@@ -29,5 +41,5 @@ except requests.exceptions.RequestException as e:
 print(f"Status code: {response.status_code}")
 print(f"Content Type: {response.headers.get("Content-Type")}")
 
-data = response.json()
-print(data["name"])
+# data = response.json()
+# print(data["name"])
