bc1eef10b599f470bafdd2adfaec0e5e69e7c1b2
dorkwitafork <hawks965@gmail.com>
2026-09-13 16:53:24 -0700

    Add Pokemon class with some method stubs. WIP

---
+++ assets/Pokemon.py
@@ -0,0 +1,22 @@
+class Pokemon():
+    def __init__(self, name, pokedex_id):
+        self.name = name
+        self.pokedex_id = pokedex_id
+        self.moves = [] 
+
+    def addMove(self, move):
+        # check if the move is learnable for that pokemon
+        # send an error if the move is not learnable.
+        if len(self.moves) < 4:
+            # add the move
+            return
+        # return an error if there is no room for the move.
+        # perhaps we can ask the user if they want to delete a move or cancel the add move entirely.
+    
+    def deleteMove(self, move):
+        # check that the move is in the list of moves for the Pokemon
+        if move in self.moves:
+            self.moves.remove(move)
+            return
+        # send an error telling the user that the pokemon does not know that move
+        
