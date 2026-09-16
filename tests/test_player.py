478ec2e96ff7b73c52e7ac0920df9e14b9b4fb9a
dorkwitafork <hawks965@gmail.com>
2026-09-16 10:16:57 -0700

    implement tests for Player class

---
+++ tests/test_player.py
@@ -0,0 +1,33 @@
+from assets.Player import Player
+from assets.Team   import Team
+
+def test_add_team_to_player():
+    player = Player("Spencer")
+    team = Team()
+
+    result = player.add_team(team)
+
+    assert result is True
+    assert team in player.teams
+    assert len(player.teams) == 1
+
+def test_delete_team_from_player():
+    player = Player("Spencer")
+    team = Team()
+
+    player.add_team(team)
+
+    result = player.delete_team(team)
+
+    assert team not in player.teams
+    assert result is True
+    assert len(player.teams) == 0
+
+def test_delete_team_not_in_player():
+    player = Player("Spencer")
+    team = Team()
+
+    result = player.delete_team(team)
+
+    assert result is False
+    assert player.teams == []
