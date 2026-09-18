84624d11d7f4baea2f785637b3946576919fea78
dorkwitafork <hawks965@gmail.com>
2026-09-18 13:59:16 -0700

    cleaned up choose_pokemon function and remove some tests in main

--- pokeapp.py
+++ pokeapp.py
@@ -19,14 +19,6 @@ def main():
         save = input("Do you want to save this team for later? (Y/N) -> ")
     print("Cool, we got out of that.\n")
 
-    print("Testing some Pokemon stuff")
-    print(f"Team: {team.get_pokemon_names()}")
-    test_pokemon = team.get_team()[0]
-    print(f"Pokemon test name: {test_pokemon.get_name()}")
-    print("Let's test adding a move")
-    for i in range(5):
-        choose_move_for_pokemon(test_pokemon)
-
 
 if __name__ == '__main__':
     main()
--- workflows/pokemon_actions.py
+++ workflows/pokemon_actions.py
@@ -5,13 +5,12 @@ from models.Pokemon import Pokemon
 # 
 
 # functions to validate moves
-def choose_move_for_pokemon(pokemon):
+def choose_move_for_pokemon(pokemon) -> bool:
     move = input("Which move should it learn? ").strip().lower()
-
-    if not pokemon.move_is_learnable(move):
-        print(f"{pokemon.get_name()} cannot learn {move}.")
-        return
 
     if pokemon.add_move(move):
         print(f"{pokemon.get_name()} learned {move}!")
-        return
+        return True
+
+    print(f"{pokemon.get_name()} could not learn {move}.")
+    return False
